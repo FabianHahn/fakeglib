961eb32519ff4570d696986e1da00df800d0f0ed
Fabian Hahn <fabian@hahn.graphics>
2016-07-31 13:11:59 +0100

    added g_queue_new, g_queue_free, g_queue_free_full and g_queue_init

--- src/GQueue.cpp
+++ src/GQueue.cpp
@@ -1,1 +1,29 @@
 #include "GQueue.h"
+
+FAKEGLIB_API GQueue *g_queue_new(void)
+{
+	GQueue *queue = new GQueue{};
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->length = 0;
+	return queue;
+}
+
+FAKEGLIB_API void g_queue_free(GQueue *queue)
+{
+	g_list_free(queue->head);
+	delete queue;
+}
+
+FAKEGLIB_API void g_queue_free_full(GQueue *queue, GDestroyNotify freeFunc)
+{
+	g_list_free_full(queue->head, freeFunc);
+	delete queue;
+}
+
+FAKEGLIB_API void g_queue_init(GQueue *queue)
+{
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->length = 0;
+}
---
+++ test/GQueue.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <glib.h>
+
+#include <vector>
+
+static std::vector<gpointer> freeCallbacks;
+
+class GQueueTest : public ::testing::Test {
+public:
+	virtual void SetUp()
+	{
+		queue = g_queue_new();
+	}
+
+	virtual void TearDown()
+	{
+		if(queue != NULL) {
+			g_queue_free(queue);
+		}
+	}
+
+	GQueue *queue;
+};
+
+static void test_free_callback(gpointer data)
+{
+	freeCallbacks.push_back(data);
+}
+
+TEST_F(GQueueTest, freeFull)
+{
+	int testData1 = 42;
+	int testData2 = 1337;
+
+	GList *list = NULL;
+	list = g_list_append(list, &testData1);
+	list = g_list_append(list, &testData2);
+	queue->head = list;
+	queue->tail = list->next;
+	queue->length = 2;
+
+	g_queue_free_full(queue, test_free_callback);
+	std::vector<gpointer> expectedCallbacks = {&testData1, &testData2};
+	ASSERT_EQ(expectedCallbacks, freeCallbacks) << "actual callback list should match expected";
+
+	queue = NULL;
+}
+
+TEST_F(GQueueTest, init)
+{
+	GList dummyList1;
+	GList dummyList2;
+
+	GQueue staticQueue;
+	staticQueue.head = &dummyList1;
+	staticQueue.tail = &dummyList2;
+	staticQueue.length = 27;
+
+	g_queue_init(&staticQueue);
+	ASSERT_TRUE(staticQueue.head == NULL) << "init should set queue head to NULL";
+	ASSERT_TRUE(staticQueue.tail == NULL) << "init should set queue tail to NULL";
+	ASSERT_EQ(0, staticQueue.length) << "init should set queue length to zero";
+}
+
