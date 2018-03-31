5200da0dc1ba2b45a372224aad673ff1f0bf7941
Fabian Hahn <fabian@hahn.graphics>
2018-03-31 14:56:34 +0100

    separated src and include directories for fakeglib

---
+++ lib/include/glib.h
@@ -0,0 +1,12 @@
+#ifndef FAKEGLIB_GLIB_H
+#define FAKEGLIB_GLIB_H
+
+#include <fakeglib/GDateTime.h>
+#include <fakeglib/GHashTable.h>
+#include <fakeglib/GList.h>
+#include <fakeglib/GMemory.h>
+#include <fakeglib/GQueue.h>
+#include <fakeglib/GString.h>
+#include <fakeglib/GTypes.h>
+
+#endif
